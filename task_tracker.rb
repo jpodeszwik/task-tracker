33b2074653e19b7aef1ce5c218865ab2cb551389
Jakub Podeszwik <jakubpodeszwik@gmail.com>
2015-06-17 22:11:00 +0200

    Adding new action

--- task_tracker.rb
+++ task_tracker.rb
@@ -1,6 +1,27 @@
 require 'Qt4'
 require './db.rb'
 
+class InputTaskWindow < Qt::Widget
+    signals 'new_task(QString)'
+
+    def initialize
+        super
+        
+        setWindowTitle "Input task"
+
+        edit = Qt::LineEdit.new self
+        button = Qt::PushButton.new 'Add', self
+        self.connect(button, SIGNAL('clicked()')) do
+            emit new_task edit.text
+            self.hide
+        end
+    
+        button.move(0, 30)
+
+        show
+    end
+end
+
 class IconSet
     def initialize
         @imgs = [Qt::Icon.new('1.png'), Qt::Icon.new('2.png'), Qt::Icon.new('3.png')] 
@@ -35,6 +56,10 @@ class Actions
         end
     end
 
+    def reload
+        @actions = Activity.all
+    end
+
     def stopCurrent
         @currentTimes.stop = Time.now
         @currentTimes.save
@@ -56,24 +81,52 @@ class Actions
 end
 
 class TrayMenu < Qt::Menu
+    slots 'new_task(QString)'
+
     def initialize
         super
         @actions = Actions.new
         @actions.each do |action|
-            act = self.addAction action.name
-            act.connect(act, SIGNAL('triggered()')) do
-                puts 'Current: ' + @actions.getCurrent.name
-                @actions.setCurrent action
-                puts 'New: ' + action.name
-            end
+            self.setActionsBehaviour action
         end
+        
+        self.addNewActivityAction
+
         self.addExitAction
+    end
+
+    def setActionsBehaviour action
+        act = self.addAction action.name
+        act.connect(act, SIGNAL('triggered()')) do
+            puts 'Current: ' + @actions.getCurrent.name
+            @actions.setCurrent action
+            puts 'New: ' + action.name
+        end
+    end
+
+    def newActionWindow
+        InputTaskWindow.new
     end
 
     def addAction(text)
         action = Action.new text, self
         super(action)
         action
+    end
+
+    def new_task(name)
+        activity = Activity.create(:name => name)
+        @actions.reload
+        self.setActionsBehaviour activity
+        puts 'New task: ' + name
+    end
+
+    def addNewActivityAction
+        activityAction = self.addAction 'New Activity'
+        activityAction.connect(activityAction, SIGNAL('triggered()')) do
+            actionWindow = self.newActionWindow
+            actionWindow.connect(actionWindow, SIGNAL('new_task(QString)'), self, SLOT("new_task(QString)"))
+        end
     end
 
     def addExitAction
